dc5a03813ddce85e992b8cc384888a2b512cb4e8
NikitaRomanenko <romanenko.nikita@pdffiller.team>
2020-09-24 16:40:48 +0300

    start actions screen

---
+++ app/src/main/java/com/getbewarned/connectinterpreter/ui/ActionsMenuActivity.java
@@ -0,0 +1,12 @@
+package com.getbewarned.connectinterpreter.ui;
+
+import android.os.Bundle;
+
+import com.getbewarned.connectinterpreter.R;
+
+public class ActionsMenuActivity extends NoStatusBarActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_new_v2);
+    }
+}
